ee5f66cd6192a16378a3e4e661917d538b60c397
Klymenko Illia <sovuh2703@gmail.com>
2023-10-30 10:28:58 +0200

    feat: Mark & parametrize

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 import source.my_functions as my_fnc
 
 
@@ -20,3 +21,17 @@ def test_divide():
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         my_fnc.divide(4, 0)
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_fnc.divide(4, 2)
+    assert result == 2
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    assert my_fnc.add(1, 2) == 3
+
+@pytest.mark.xfail(reason='We know we cannot divide by zero')
+def test_devide_zero_broken():
+    my_fnc.divide(3, 0)
